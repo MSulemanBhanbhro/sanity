b11ae819e8cfcb944d441a65bf0c71c1e70979aa
MSulemanBhanbhro <ms5901562@gmail.com>
2024-12-29 00:56:27 +0500

    chnages

--- src/sanity/lib/urlFor.ts
+++ src/sanity/lib/urlFor.ts
@@ -3,6 +3,6 @@ import { client } from "./client";
 
 const builder = imageUrlBuilder(client);
 
-export function urlFor(source: any) {
+export function urlFor(source:Post) {
   return builder.image(source);
 }
